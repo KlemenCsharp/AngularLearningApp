35c837e7e5f1706e179268e1bed2b4f88bb985e1
Klemen Zupan <klemen.zupan@inden.si>
2022-01-30 22:50:05 +0100

    Created simple animation

--- src/app/features/animations/page-animations/page-animations.component.ts
+++ src/app/features/animations/page-animations/page-animations.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-page-animations',
   templateUrl: './page-animations.component.html',
-  styleUrls: ['./page-animations.component.scss']
+  styleUrls: ['./page-animations.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.8,
+        backgroundColor: 'blue'
+      })),
+      transition('open => closed', [
+        animate(1000)
+      ]),
+      transition('closed => open', [
+        animate(500)
+      ]),
+    ]),
+  ],
 })
 export class PageAnimationsComponent implements OnInit {
-
-  constructor() { }
+isOpen = true;
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
 
 }
